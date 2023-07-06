611229dceb7c0565b2acb2c94b7553e57dd90090
KotamartiSriVidya <sri_vidya.kotamarti@mercedes-benz.com>
2023-07-06 11:28:58 +0530

    Initial Commit

---
+++ blog/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+class Blog(models.Model):
+    title = models.CharField(max_length = 200)
+    date = models.DateField(auto_now_add = True)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+
+    
+
